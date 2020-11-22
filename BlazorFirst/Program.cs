c126d019526ae9bd67fdf54c9859fb3ed3b98c2c
xiaozhi8882001 <xiaozhi8882001@hotmail.com>
2020-11-22 21:35:24 +0800

    ss

--- BlazorFirst/Program.cs
+++ BlazorFirst/Program.cs
@@ -17,8 +17,11 @@ namespace BlazorFirst
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
             builder.Services.AddAntDesign();
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
 
             await builder.Build().RunAsync();
         }
