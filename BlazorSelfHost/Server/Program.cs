a898abaaec7004019f86d1258b536524a9f8b61f
肖志 <xiaozhi8882001@hotmail.com>
2020-11-24 16:18:31 +0800

    ss

---
+++ BlazorSelfHost/Server/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorSelfHost.Server
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            //启动前读取端口配置，不用再用外部批处理文件启动来传递端口号参数
+            var configuration = new ConfigurationBuilder().SetBasePath(AppContext.BaseDirectory)
+                             .AddJsonFile("host.json")
+                             .Build();
+            var url = configuration["urls"];
+
+            var host = Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseUrls(url);
+                    webBuilder.UseStartup<Startup>();
+                });
+
+            return host;
+        }
+    }
+}
